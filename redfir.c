60a12ba764d7386fbbb885f1472bd1ae8987244f
Aki Nyrhinen <anyrhine@gmail.com>
2024-10-09 08:56:49 -0700

    modified to use kaiser window, with stopband attenuation and transition bandwidth as parameters

--- redfir.c
+++ redfir.c
@@ -6,6 +6,8 @@
 #include <string.h>
 
 typedef struct RedFir RedFir;
+typedef struct RedFirKaiser RedFirKaiser;
+
 struct RedFir {
 	double *imp; // filter impulse
 	double *samples; // ring buffer, 2*len
@@ -13,32 +15,82 @@ struct RedFir {
 	int off; // next sample index
 };
 
+struct RedFirKaiser {
+	double beta;
+	double bessel_beta;
+	int order;
+};
+
+// 0th order modified bessel function of the first kind.
+// this isn't optimized for numerical performance, but
+// it seems to work well enough.
 static double
-blackman_window(int i, int n)
+bessel0(double x)
+{
+	double kfac = 1.0;
+	double halfx = 0.5*x;
+	double sum = 0.0;
+	for(int k = 0; k < 20; k++){
+		kfac *= k;
+		if(kfac == 0)
+			kfac = 1;
+		double term = pow(halfx, k) / kfac;
+		sum += term*term;
+	}
+	return sum;
+}
+
+// double att = -6.0*log2(ripple);
+// there's only one set of parameters for attenuations >50dB..
+// attenuation beyond 21dB doesn't impact filter order.
+// what's the tradeoff between less and more attenuation then?
+// more attenuation should widen the transition band.. which means
+// for the same transition we should need a wider filter.. but not?
+
+static void
+kaiser_fit(RedFirKaiser *kp, double transbw, double sfreq, double att_db)
+{
+	double tw = 2.0 * M_PI * transbw / sfreq;
+
+	double beta;
+	if(att_db <= 21.0){
+		beta = 0.0;
+	} else if(att_db <= 50.0){
+		beta = 0.5842 * pow(att_db-21.0, 0.4) + 0.07886 * (att_db - 21.0);
+	} else {
+		beta = 0.1102 * (att_db - 8.7);
+	}
+
+	kp->beta = beta;
+	kp->bessel_beta = bessel0(beta);
+	// make sure it's odd, we use spectral inversion.
+	kp->order = 1 | (int)ceil((att_db-7.95)/(2.285*tw));
+}
+
+// needs to be tested
+static double
+kaiser_window(void *arg, int i, int n)
+{
+	RedFirKaiser *kp = (RedFirKaiser*)arg;
+	double a = (n-1)/2.0;
+	double naa = (i-a)/a;
+	double naa2 = naa*naa;
+	return bessel0(kp->beta * sqrt(1.0 - naa2)) / kp->bessel_beta;
+}
+
+static double
+blackman_window(void *arg, int i, int n)
 {
 	double rcp_n = 1.0f / (n-1);
 	return 0.42f - 0.5f*cos(2.0f*M_PI*i*rcp_n) + 0.08f*cos(4.0f*M_PI*i*rcp_n);
 }
 
 static double
-blackman_nuttall_window(int i, int n)
-{
-	double a0 = 0.3635819f;
-	double a1 = 0.4891775f;
-	double a2 = 0.1365995f;
-	double a3 = 0.0106411f;
-	double rcp_n = 1.0f / (n-1);
-	double wn = a0 - a1 * cos(2.0*M_PI*i*rcp_n) + a2 * cos(4.0f*M_PI*i*rcp_n) - a3 * cos(6.0*M_PI*i*rcp_n);
-	return wn;
-}
-
-static double
-blackman_harris_window(int i, int n)
+blackman_harris_window(void *arg, int i, int n)
 {
 	double rcp_n = 1.0f / (n-1);
 	return 0.35875f - 0.48829f*cos(2.0f*M_PI*i*rcp_n) + 0.14128f*cos(4.0f*M_PI*i*rcp_n) - 0.01168f*cos(6.0f*M_PI*i*rcp_n);
 }
-
 
 static double
 sinc(double freq_ratio, int i, int n)
@@ -79,12 +131,11 @@ redfir_free(RedFir *fp)
 }
 
 void
-redfir_window(RedFir *ap, double (*winfunc)(int i, int n))
+redfir_window(RedFir *ap, double (*winfunc)(void *arg, int i, int n), void *arg)
 {
 	for(int i = 0; i < ap->len; i++)
-		ap->imp[i] = (*winfunc)(i, ap->len);
+		ap->imp[i] = (*winfunc)(arg, i, ap->len);
 }
-
 
 void
 redfir_sinc(RedFir *ap, double freq_ratio)
@@ -129,6 +180,24 @@ redfir_inverse(RedFir *dst, RedFir *ap)
 }
 
 int
+redfir_reverse(RedFir *dst, RedFir *ap)
+{
+	if(dst->len == ap->len){
+		int i;
+		for(i = 0; i < ap->len; i++){
+			double tmp;
+			if((i&1) != 0)
+				tmp = -ap->imp[i];
+			else
+				tmp = ap->imp[i];
+			dst->imp[i] = tmp;
+		}
+		return 0;
+	}
+	return -1;
+}
+
+int
 redfir_normalize(RedFir *dst, RedFir *ap)
 {
 	if(dst->len == ap->len){
@@ -143,8 +212,10 @@ redfir_normalize(RedFir *dst, RedFir *ap)
 	return -1;
 }
 
-// [ 0 1 2 3 4 0 1 2 3 4 ]
-//   x y z w v x y z w v
+// the ring buffer is implemented by inserting each sample at two locations,
+// the current offset and also the filter length past the current offset.
+// the buffer needs must be twice the filter length, but there's no modulo
+// arithmetic in the loop (should be easy simd)
 double
 redfir_step(RedFir *fp, double sample)
 {
@@ -212,8 +283,7 @@ redfir_freqplot(FILE *fp, RedFir *ap)
 	double ratio = powf(2.0f, 1.0f/24.0f);
 	enum { InputLen = 8192 };
 	double *input = malloc(InputLen * sizeof input[0]);
-//	for(double freq = 13.75f; freq < 10000.0f; freq *= ratio){
-	for(double freq = 20.0f; freq < 10000.0f; freq += 25.0f){
+	for(double freq = 50.0f; freq < 22050.0f; freq += 50.0f){
 		redfir_genfreq(input, InputLen, freq / 44100.0f);
 		redfir_reset(ap);
 		double maxout = 0.0f;
@@ -230,35 +300,44 @@ void
 redfir_printf(FILE *fp, RedFir *ap)
 {
 	for(int i = 0; i < ap->len; i++){
-		fprintf(fp, "%d %.6f\n", i, ap->imp[i]);
+		fprintf(fp, "%d %.20f\n", i, ap->imp[i]);
 	}
 }
 
 int
 main(int argc, char *argv[])
 {
-	RedFir win;
+	FILE *fp = NULL;
+	RedFir window;
 	RedFir lowpass;
 	RedFir highpass;
+	RedFir bafflestep;
 
 	int freq = strtol(argv[1], NULL, 10);
-	int flen = strtol(argv[2], NULL, 10);
+	int att_db = strtol(argv[2], NULL, 10);
+	int transbw = strtol(argv[3], NULL, 10);
 
-	redfir_init(&lowpass, flen);
-	redfir_init(&highpass, flen);
-	redfir_init(&win, flen);
+	RedFirKaiser kaiser;
+	kaiser_fit(&kaiser, transbw, 44100.0, att_db);
+	fprintf(stderr, "# kaiser filter for %dhz band with -%ddB stop banda\n", transbw, att_db);
+	fprintf(stderr, "# filter order %d\n", kaiser.order);
 
-	//redfir_window(&win, &blackman_nuttall_window);
-	redfir_window(&win, &blackman_harris_window);
+	redfir_init(&window, kaiser.order);
+	redfir_window(&window, &kaiser_window, (void*)&kaiser);
+
+	redfir_init(&lowpass, window.len);
+	redfir_init(&highpass, window.len);
+	redfir_init(&bafflestep, window.len);
+
 	redfir_sinc(&lowpass, freq / 44100.0f);
-	redfir_mul(&lowpass, &lowpass, &win);
+	redfir_mul(&lowpass, &lowpass, &window);
 	redfir_normalize(&lowpass, &lowpass);
 	redfir_inverse(&highpass, &lowpass);
 
-	double att_db = 96.0;
-	fprintf(stderr, "# estimated order %.1f\n", 44100.0f / freq);
+	redfir_scale(&bafflestep, &highpass, 0.5); // -6dB
+	redfir_add(&bafflestep, &bafflestep, &lowpass);
 
-	FILE *fp = fopen("lp-freq.txt", "wb");
+	fp = fopen("lp-freq.txt", "wb");
 	redfir_freqplot(fp, &lowpass);
 	fclose(fp);
 
@@ -266,12 +345,24 @@ main(int argc, char *argv[])
 	redfir_freqplot(fp, &highpass);
 	fclose(fp);
 
+	fp = fopen("bs-freq.txt", "wb");
+	redfir_freqplot(fp, &bafflestep);
+	fclose(fp);
+
 	fp = fopen("lowpass.txt", "wb");
 	redfir_printf(fp, &lowpass);
 	fclose(fp);
 
+	fp = fopen("highpass.txt", "wb");
+	redfir_printf(fp, &highpass);
+	fclose(fp);
+
+	fp = fopen("bafflestep.txt", "wb");
+	redfir_printf(fp, &bafflestep);
+	fclose(fp);
+
 	fp = fopen("window.txt", "wb");
-	redfir_printf(fp, &win);
+	redfir_printf(fp, &window);
 	fclose(fp);
 
 	// generate step response and spit it out.
@@ -291,7 +382,7 @@ main(int argc, char *argv[])
 
 	redfir_free(&lowpass);
 	redfir_free(&highpass);
-	redfir_free(&win);
+	redfir_free(&window);
 
 	return 0;
 }
